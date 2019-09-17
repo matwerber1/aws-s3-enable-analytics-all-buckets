946494eb0319aec8113f4db6f6d1a1ca1da615fc
Mat Werber <werberm@amazon.com>
2019-09-17 09:20:58 -0400

    check for proper bucket regions

--- tool.js
+++ tool.js
@@ -2,18 +2,51 @@ const aws = require('aws-sdk');
 const s3 = new aws.S3();
 
 // UPDATE AS NEEDED
-const analytics_bucket = 'werberm-s3-tests-logs';
-const analytics_prefix = 's3_analytics'
+const report_prefix = 's3_analytics'
+const report_bucket = 'werberm-s3-tests-logs';
 //const analytics_log_bucket_account_id = "999999999999";
+
+//#https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
 
 async function main() {
   
+  var report_destination_region = await getBucketRegion(report_bucket);
   // the async is also helpful for avoiding throttling limits
   var listBucketsResponse = await s3.listBuckets().promise();
+
   await asyncForEach(listBucketsResponse.Buckets, async (bucket) => {
-    await EnableS3Analytics(bucket.Name);
+    var bucket_region = await getBucketRegion(bucket.Name);
+    if (bucket_region !== report_destination_region) {
+      console.log(
+        `Bucket ${bucket.Name} (${bucket_region}) must be in same region as reporting `
+        + `bucket ${report_bucket} (${report_destination_region})for delivery of S3 `
+        + `analytics. Skipping analytics config...\n`
+      );
+    }
+    else {
+      await EnableS3Analytics(bucket.Name);
+    }
   });
 
+}
+
+
+async function getBucketRegion(bucket) {
+
+  var getLocationResponse = await s3.getBucketLocation({ Bucket: bucket }).promise();
+  var bucket_region = getLocationResponse.LocationConstraint;
+
+  // Some "LocationConstraints" do not map 1-to-1 to the typical region codes we are familiar with:
+  // https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_region
+  switch (bucket_region) {
+    case "":
+      bucket_region = 'us-east-1';
+      break;
+    case "EU":
+      bucket_region = 'eu-west-1';
+      break;    
+  }
+  return bucket_region;
 }
 
 async function asyncForEach(array, callback) {
@@ -26,8 +59,8 @@ async function EnableS3Analytics(target_bucket) {
 
   var analytics_config_id = 'analytics-config';
   var target_bucket_arn = `arn:aws:s3:::${target_bucket}`;
-  var analytics_bucket_arn = `arn:aws:s3:::${analytics_bucket}`;
-  var full_prefix = `${analytics_prefix}/bucket=${target_bucket}`;
+  var report_bucket_arn = `arn:aws:s3:::${report_bucket}`;
+  var full_prefix = `${report_prefix}/bucket=${target_bucket}`;
 
   var params = {
     AnalyticsConfiguration: { 
@@ -36,7 +69,7 @@ async function EnableS3Analytics(target_bucket) {
         DataExport: {
           Destination: { 
             S3BucketDestination: { 
-              Bucket: analytics_bucket_arn,
+              Bucket: report_bucket_arn,
               Format: "CSV",
               //BucketAccountId: analytics_log_bucket_account_id,
               Prefix: full_prefix
